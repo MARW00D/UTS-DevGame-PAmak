2b9963031f936a2b5d14ef619122ad2b92a3a930
MARW00D <48198367+MARW00D@users.noreply.github.com>
2021-05-01 01:00:13 +0700

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D)) {
+            anim.SetBool("hajar",true);
+        } else {
+            anim.SetBool("hajar",false);
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            anim.SetBool("sepak",true);
+        } else {
+            anim.SetBool("sepak",false);
+        }
+    }
+}
